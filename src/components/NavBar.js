954490c5e5834c50787026bb92c0b89958d3dcf3
Chanaka97n <chanaka97n@gmail.com>
2023-06-06 00:30:52 +0530

    added  finalized one

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,11 +11,11 @@ function NavBar() {
       <h5
         className="text-base text-blue-800 text-center p-2 px-5 cursor-pointer hover:opacity-50"
         onClick={() =>
-          navigate(`${location.pathname == "/login" ? "/" : "/login"}
+          navigate(`${location.pathname === "/login" ? "/" : "/login"}
         `)
         }
       >
-        {location.pathname == "/login" ? "Home" : "Login"}
+        {location.pathname === "/login" ? "Home" : "Login"}
       </h5>
     </div>
   );
